d580b98a41474a411248f6a625990ef734df6b4d
Magus <magus@bgforge.net>
2021-08-01 21:37:26 +0700

    rename dat

--- extra/package.sh
+++ extra/package.sh
@@ -6,7 +6,6 @@ bin_dir="$(realpath extra/bin)"
 dat2a="wine $bin_dir/dat2.exe a -1"
 trans_dir="$(realpath translations)"
 file_list="../file.list"
-mod_name="upu_sound"
 short_sha="$(git rev-parse --short HEAD)"
 # defaults, local build or github non-tagged
 version="git$short_sha"
@@ -18,14 +17,11 @@ if [[ ! -z "${GITHUB_REF-}" ]]; then # github build
   fi
 fi
 
-# data
-dat="$mod_name.dat"
-
 cd "$trans_dir"
 rm -f file.list
 for lang in $(ls); do
   cd "$lang"
-  dat="${mod_name}_${lang}_${version}.dat"
+  dat="upu_${lang}_sound.dat"
   # I don't know how to pack recursively
   find . -type f | sed -e 's|^\.\/||' -e 's|\/|\\|g' | sort > "$file_list" # replace slashes with backslashes
   $dat2a "../$dat" @"$file_list" 2>&1 | grep -v "wine: Read access denied for device" # wine pollutes the log
